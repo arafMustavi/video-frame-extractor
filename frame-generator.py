789f789c279c7ecabef3b99caa6d99ec0bfccb36
arafmustavi <arafmustavi@gmail.com>
2021-03-10 10:37:33 +0600

    :+1: generation of frames in directory support added

--- frame-generator.py
+++ frame-generator.py
@@ -1,15 +1,20 @@
 import cv2
+import os
+
+dirName = 'extracted-frames'
+try:
+    os.mkdir(dirName)
+    print("Directory " , dirName ,  " Created ") 
+except FileExistsError:
+    print("Directory " , dirName ,  " already exists")
 
 vidcap = cv2.VideoCapture('sample-input.mp4')
-success,image = vidcap.read()
+
+success,image = vidcap.read()   
 frame_count = 0
 
 while success:
-    # path = 'extracted/' + str(frame_count) + '.jpg'
-    path = 'Path/Image.jpg'
-    # cv2.imwrite("frame{}.jpg".format(frame_count), image)     # save frame as JPEG file      
-    cv2.imwrite(path, image)     # save frame as JPEG file      
-
+    cv2.imwrite('extracted-frames/frame{}.jpg'.format(frame_count), image)     
     success,image = vidcap.read()
     print('Read a new frame:', success,'Next Frame Number: {}'.format(frame_count))
     frame_count += 1
