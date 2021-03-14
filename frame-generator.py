a31bdd2a58a9154b34f26406abd1b54bb555dbf2
arafmustavi <arafmustavi@gmail.com>
2021-03-15 02:31:15 +0600

    :100: fps information added

--- frame-generator.py
+++ frame-generator.py
@@ -13,16 +13,20 @@ try:
 except FileExistsError:
     print("Directory " , dirName ,  " already exists")
 
+
 # extract frames from a video by processing one video at a time
 for sample_clip in video_clip_list:
-    video_path = 'sample-clips/' + str(sample_clip)
-    
+    video_path = 'sample-clips/' + str(sample_clip)    
     vidcap = cv2.VideoCapture(video_path)
+# get frames-per-second information
+    fps = vidcap.get(cv2.CAP_PROP_FPS)
+    print ("Frames per second for {} is : {}".format(sample_clip,int(fps)))
+  
     success,image = vidcap.read()   
     frame_count = 0
-
     while success:
         cv2.imwrite('extracted-frames/{}-frame{}.jpg'.format(sample_clip,frame_count), image)     
         success,image = vidcap.read()
         frame_count += 1
-    print('Total {} frames has been extracted from {}'.format(frame_count,sample_clip))
+    print('Total {} frames has been extracted from {}\n'.format(frame_count,sample_clip))
+    vidcap.release()
